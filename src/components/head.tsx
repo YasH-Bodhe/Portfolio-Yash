e1c137806751fbf1abc381946a4f388457f74bc2
Yash Bodhe <bodheyash@gmail.com>
2025-11-03 11:47:46 +0530

    Changed Thumbnail

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -16,10 +16,10 @@ export function Head({ metadata }: { metadata: Metadata }) {
         property='og:description'
         content={metadata.description?.toString()}
       />
-      <meta property='og:url' content='https://notharshhaa.site' />
+      <meta property='og:url' content='https://portfolio-yash-azure.vercel.app/' />
       <meta
         property='og:image'
-        content='https://notharshhaa.site/opengraph-image.png'
+        content='public\opengraph-image.png'
       />
       <meta property='og:image:width' content='1200' />
       <meta property='og:image:height' content='630' />
